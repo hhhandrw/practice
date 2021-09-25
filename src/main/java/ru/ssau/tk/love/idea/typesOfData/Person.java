ef95e18bd51d54ecb932faf150f3a1686b4d32fc
Khlebodarov <andrw.h2002@mail.ru>
2021-09-25 14:58:37 +0300

    Создан класс Person, к нему добавлены поля, к ним геттеры и сеттеры. Созданы объекты класса и заполнены поля

---
+++ src/main/java/ru/ssau/tk/love/idea/typesOfData/Person.java
@@ -0,0 +1,63 @@
+package ru.ssau.tk.love.idea.typesOfData;
+
+public class Person {
+    public String firstName;
+    public String lastName;
+    public int passportId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
+    public static void main(String[] args) {
+        Person firstPerson = new Person();
+
+        firstPerson.setFirstName("Andrey");
+        firstPerson.setLastName("Khlebodarov");
+        firstPerson.setPassportId(1);
+
+        Person secondPerson = new Person();
+
+        secondPerson.setFirstName("Tom");
+        secondPerson.setLastName("Hardy");
+        secondPerson.setPassportId(612);
+
+        Person thirdPerson = new Person();
+
+        thirdPerson.setFirstName("Cristiano");
+        thirdPerson.setLastName("Ronaldo");
+        thirdPerson.setPassportId(7);
+
+        System.out.println(firstPerson.getFirstName());
+        System.out.println(firstPerson.getLastName());
+        System.out.println(firstPerson.getPassportId());
+
+        System.out.println(secondPerson.getFirstName());
+        System.out.println(secondPerson.getLastName());
+        System.out.println(secondPerson.getPassportId());
+
+        System.out.println(thirdPerson.getFirstName());
+        System.out.println(thirdPerson.getLastName());
+        System.out.println(thirdPerson.getPassportId());
+    }
+}
