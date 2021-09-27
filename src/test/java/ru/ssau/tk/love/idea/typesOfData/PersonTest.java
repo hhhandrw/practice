de88dfda48c6af6f533a21b74d8447bc8506b8f8
Khlebodarov <andrw.h2002@mail.ru>
2021-09-27 10:57:19 +0300

    Класс Person покрыт тестами

---
+++ src/test/java/ru/ssau/tk/love/idea/typesOfData/PersonTest.java
@@ -0,0 +1,38 @@
+package ru.ssau.tk.love.idea.typesOfData;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void testGetAndSet() {
+        Person firstPerson = new Person();
+        Person secondPerson = new Person();
+        Person thirdPerson = new Person();
+
+        firstPerson.setFirstName("Andrey");
+        firstPerson.setLastName("Khlebodarov");
+        firstPerson.setPassportId(1);
+
+        secondPerson.setFirstName("Tom");
+        secondPerson.setLastName("Hardy");
+        secondPerson.setPassportId(612);
+
+        thirdPerson.setFirstName("Cristiano");
+        thirdPerson.setLastName("Ronaldo");
+        thirdPerson.setPassportId(7);
+
+        assertEquals(firstPerson.getFirstName(), "Andrey");
+        assertEquals(firstPerson.getLastName(), "Khlebodarov");
+        assertEquals(firstPerson.getPassportId(), 1);
+
+        assertEquals(secondPerson.getFirstName(), "Tom");
+        assertEquals(secondPerson.getLastName(), "Hardy");
+        assertEquals(secondPerson.getPassportId(), 612);
+
+        assertEquals(thirdPerson.getFirstName(), "Cristiano");
+        assertEquals(thirdPerson.getLastName(), "Ronaldo");
+        assertEquals(thirdPerson.getPassportId(), 7);
+    }
+}
