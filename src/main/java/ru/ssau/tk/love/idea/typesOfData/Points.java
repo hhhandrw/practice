0dca9257926ad7540c65cbcaef5e786136505e3a
Khlebodarov <andrw.h2002@mail.ru>
2021-09-28 20:44:32 +0300

    Создан класс Points с 4 методами и конструктором

---
+++ src/main/java/ru/ssau/tk/love/idea/typesOfData/Points.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.love.idea.typesOfData;
+
+public class Points {
+    private Points() {
+    }
+
+    public static Point sum(Point firstPoint, Point secondPoint) {
+        return new Point(firstPoint.x + secondPoint.x, firstPoint.y + secondPoint.y, firstPoint.z + secondPoint.z);
+    }
+
+    public static Point subtract(Point firstPoint, Point secondPoint) {
+        return new Point(firstPoint.x - secondPoint.x, firstPoint.y - secondPoint.y, firstPoint.z - secondPoint.z);
+    }
+
+    public static Point divide(Point firstPoint, Point secondPoint) {
+        if (secondPoint.x == 0 || secondPoint.y == 0 || secondPoint.z == 0) {
+            throw new ArithmeticException("Dividing by zero");
+        }
+        return new Point(firstPoint.x / secondPoint.x, firstPoint.y / secondPoint.y, firstPoint.z / secondPoint.z);
+    }
+
+    public static Point multiply(Point firstPoint, Point secondPoint) {
+        return new Point(firstPoint.x * secondPoint.x, firstPoint.y * secondPoint.y, firstPoint.z * secondPoint.z);
+    }
+}
