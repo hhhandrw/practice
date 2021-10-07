b5c280d631bb448635ceaf5389f45633a7d9a898
Khlebodarov <andrw.h2002@mail.ru>
2021-10-07 22:46:58 +0400

    2.2	Создать метод, генерирующий массив чисел по входной размерности. Массив должен быть заполнен единицами, кроме первого и последнего элемента – на их месте должны быть двойки. Метод должен возвращаться созданный массив. Покрыть метод тестами.

--- src/main/java/ru/ssau/tk/love/idea/arrays/Array.java
+++ src/main/java/ru/ssau/tk/love/idea/arrays/Array.java
@@ -1,7 +1,17 @@
 package ru.ssau.tk.love.idea.arrays;
 
 public class Array {
-    public static int[] newArray(int size) {
+    public static int[] createArray(int size) {
         return new int[size];
+    }
+
+    public static int[] generateArray(int size) {
+        int[] array = new int[size];
+        for (int i = 1; i < size - 1; i++) {
+            array[i] = 1;
+        }
+        array[0] = 2;
+        array[size - 1] = 2;
+        return array;
     }
 }
--- src/test/java/ru/ssau/tk/love/idea/arrays/ArrayTest.java
+++ src/test/java/ru/ssau/tk/love/idea/arrays/ArrayTest.java
@@ -6,9 +6,20 @@ import static org.testng.Assert.*;
 
 public class ArrayTest {
     @Test
-    public void newArrayTest(){
-        assertEquals(Array.newArray(5), new int[5]);
-        assertEquals(Array.newArray(15), new int[15]);
-        assertEquals(Array.newArray(0), new int[0]);
+    public void createArrayTest() {
+        assertEquals(Array.createArray(5), new int[5]);
+        assertEquals(Array.createArray(15), new int[15]);
+        assertEquals(Array.createArray(0), new int[0]);
+    }
+
+    @Test
+    public void generateArrayTest() {
+        int[] referenceArray = new int[]{2, 1, 1, 1, 2};
+
+        assertEquals(referenceArray[0], Array.generateArray(5)[0]);
+        assertEquals(referenceArray[1], Array.generateArray(5)[1]);
+        assertEquals(referenceArray[2], Array.generateArray(5)[2]);
+        assertEquals(referenceArray[3], Array.generateArray(5)[3]);
+        assertEquals(referenceArray[4], Array.generateArray(5)[4]);
     }
 }
