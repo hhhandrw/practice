9f8b900cf92f8d668f3175fd45c0532371e38b99
Khlebodarov <andrw.h2002@mail.ru>
2021-10-19 22:00:29 +0400

    3.1	Создать метод, принимающий на вход строку String, и выводящий в консоль каждый символ входной строки на отдельной строке консоли. Реализовать метод с помощью цикла for, а также с использованием методов строк length() и charAt(). Нумерация символов, как и в массиве, с нуля.

---
+++ src/main/java/ru/ssau/tk/love/idea/strings/MyString.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.love.idea.strings;
+
+public class MyString {
+    public static void printString(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
---
+++ src/main/java/ru/ssau/tk/love/idea/strings/Tester.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.love.idea.strings;
+
+public class Tester {
+    public static void main(String[] args) {
+        String str = ("What a lovely day today");
+        MyString.printString(str);
+    }
+}
