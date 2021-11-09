72602d66b450a932bdd9e6c54e96fc326af4acfa
Khlebodarov <andrw.h2002@mail.ru>
2021-11-09 23:25:00 +0400

    4.2

--- src/main/java/ru/ssau/tk/love/idea/exceptions/MyClassTwo.java
+++ src/main/java/ru/ssau/tk/love/idea/exceptions/MyClassTwo.java
@@ -6,4 +6,14 @@ public class MyClassTwo {
     public static String getFirstAndLastNames(Person person) {
         return person.toString();
     }
+
+    public static char[] getArrayOfChars (String[] strings, int n) {
+        char [] chars = new char[strings.length];
+        int i = 0;
+        for (String str: strings) {
+            chars [i] = str.charAt(n);
+            i++;
+        }
+        return chars;
+    }
 }
--- src/test/java/ru/ssau/tk/love/idea/exceptions/MyClassTwoTest.java
+++ src/test/java/ru/ssau/tk/love/idea/exceptions/MyClassTwoTest.java
@@ -17,4 +17,22 @@ public class MyClassTwoTest {
             MyClassTwo.getFirstAndLastNames(null);
         });
     }
+
+    @Test
+    public void testGetArrayOfChars() {
+        String[] stringsOne = {"apple", "ice", "pear"};
+        char[] charsOne = new char[]{'p', 'c', 'e'};
+        assertEquals(MyClassTwo.getArrayOfChars(stringsOne, 1), charsOne);
+
+        char[] charsTwo = new char[]{'p', 'e', 'a'};
+        assertEquals(MyClassTwo.getArrayOfChars(stringsOne, 2), charsTwo);
+
+        Assert.assertThrows(NullPointerException.class, () -> MyClassTwo.getArrayOfChars(null, 1));
+
+        String[] stringsTwo = {null, "ice", "pear"};
+        Assert.assertThrows(NullPointerException.class, () -> MyClassTwo.getArrayOfChars(stringsTwo, 1));
+
+        Assert.assertThrows(StringIndexOutOfBoundsException.class, () -> MyClassTwo.getArrayOfChars(stringsOne, 11));
+        Assert.assertThrows(StringIndexOutOfBoundsException.class, () -> MyClassTwo.getArrayOfChars(stringsOne, -2));
+    }
 }
